914010684077adcf62413f42930c69ecc1d4fa62
thaian240414 <thaian240414@gmail.com>
2025-09-28 15:10:42 +0700

    Update gioi_thieu_ban_than.py

--- gioi_thieu_ban_than.py
+++ gioi_thieu_ban_than.py
@@ -1,7 +1,7 @@
 from guizero import App, Text, Box, Picture
 
 cuaso = App(title="Giới thiệu bản thân", width=1000, height=700,bg="#ffd6ef")
-header_box = Box(cuaso, align="middle",width="fill", border=True)
+header_box = Box(cuaso,width="fill", border=True)
 
 tieude1 = Text(header_box, text="Tên: Thái An",size=30, color="#0082d9")
 tieude2 = Text(header_box,text="Tuổi: 11",size=30, color="#0082d9")
@@ -12,3 +12,4 @@ hinhanh = Picture(header_box, image="meomeo.gif", height=400)
 tieude5 = Text(header_box, text="Xin chào, tôi tên là An. Đây là chương trình giới thiệu về tôi.")
 
 cuaso.display()
+
